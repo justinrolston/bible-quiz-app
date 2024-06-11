ec460d623ed3b5a877085ec1e0f4a05c2012e2dc
Justin Rolston <justinrolston@gmail.com>
2024-06-10 21:21:18 -0400

    update test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,11 @@
 import { describe, it, expect } from "vitest";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-describe("A truthy statement", () => {
-  it("should be equal to 2", () => {
-    expect(1 + 1).toEqual(2);
+describe("App render", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
   });
 });
